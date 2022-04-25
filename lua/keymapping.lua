3698afdfb08e9516652e311c22278d40657a443a
seulG <icoderdev@outlook.com>
2022-04-25 13:12:30 +0800

    update theme

--- lua/keymapping.lua
+++ lua/keymapping.lua
@@ -26,11 +26,10 @@ vim.api.nvim_set_keymap('n' , '<TAB>', ':bnext<CR>', { noremap = true, silent = 
 vim.api.nvim_set_keymap('n' , '<S-TAB>', ':bprevious<CR>', { noremap = true, silent = true })
 
 -- move line
-vim.api.nvim_set_keymap('x' , 'K', ':move \'>+1<CR>gv-gv\'', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('x' , 'J', ':move \'<-2<CR>gv-gv\'', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('x' , 'J', ':move \'>+1<CR>gv-gv\'', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('x' , 'K', ':move \'<-2<CR>gv-gv\'', { noremap = true, silent = true })
 
 -- tab complete
 -- vim.api.nvim_set_keymap('i' , '<expr><TAB>', 'pumvisible() ? \"<C-n>\" : \"<TAB>\"', { noremap = true, silent = true })
-
 
 vim.api.nvim_set_keymap("n", "==", "<cmd>Neoformat<CR>", { noremap = true, silent = true })
--- lua/theme/github.lua
+++ lua/theme/github.lua
@@ -1,16 +1,12 @@
 require("github-theme").setup({
-  theme_style = "light_default",
+  theme_style = "light_colorblind",
   -- set keyword style
   comment_style = "NONE",
   keyword_style = "NONE",
   function_style = "NONE",
   variable_style = "NONE",
-  
   dark_sidebar = false,
-
-  function_style = "bold",
   -- set transparent
   transparent = true,
-
   sidebars = {"qf", "vista_kind", "terminal", "packer"},
 })
